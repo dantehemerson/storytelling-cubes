ea155a306583336c63afaf14840739e8488ee0c5
dantehemerson <dantehemerson@gmail.com>
2018-10-27 10:24:09 -0500

    Add dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import styled from 'styled-components'
+import onClickOutside from 'react-onclickoutside'
+
+const Wrapper = styled.div`	
+	color: #333;
+	cursor: pointer;
+	text-transform: uppercase;
+`
+
+const Header = styled.div`
+	height: 36px;
+	display: flex;
+	box-shadow: 0 0 2px #111;
+	border-radius: 8px;
+	background: white;
+	align-items: center;
+	padding: 0 10px;
+	overflow: hidden;
+	margin-bottom: 4px;
+	border: 1px solid #dedede;
+`
+
+const Title = styled.h4`
+`
+
+const List = styled.ul`
+	background: white;
+	border-radius: 8px;
+	overflow: hidden;
+`
+
+const Item = styled.li`
+	padding: 9px 10px;
+	&:hover {
+		background: #efefef;
+	}
+`
+
+class Dropdown extends React.Component {
+	state = {
+		open: false,
+		title: 'Item1'
+	}
+
+	handleClickOutside(e) {
+		this.setState({
+			open: false
+		})
+	}
+
+	toggleList = () => {
+		this.setState(prevState => ({
+			open: !prevState.open
+		}))
+	}
+
+	render() {
+		const { list } = this.props
+		console.log(this.state.open)
+		return (
+			<Wrapper>
+				<Header onClick={ this.toggleList }>
+					<Title>{ this.state.title }</Title>
+				</Header>
+				{
+					this.state.open &&
+					<List>
+						{
+							list.map(item => 
+								<Item key={ item.id }>{ item.title }</Item>)							
+						}
+					</List>
+				}
+			</Wrapper>
+		)
+	}
+}
+
+export default onClickOutside(Dropdown)
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+import Dropdown from './Dropdown'
+
 const Container = styled.div`
 	
+`
+
+const Divisor = styled.div`
+	max-width: 300px;
+	margin: 100px auto;
 `
 
 class Home extends React.Component {
@@ -12,6 +19,16 @@ class Home extends React.Component {
 			<Container>
 				Home
 				<Link to='/about'>Go to About</Link>
+				<hr/>
+				<Divisor>
+					<Dropdown
+						list={[
+							{ id: 1, title: 'Item1' },
+							{ id: 2, title: 'Item2' },
+							{ id: 4, title: 'Item4' },
+							{ id: 3, title: 'Item3' }
+						]}/>
+				</Divisor>
 			</Container>
 		)
 	}
