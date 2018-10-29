9edb739bb2933b10ada06db397fed0f1530a1929
dantehemerson <dantehemerson@gmail.com>
2018-10-29 11:31:08 -0500

    :hammer: Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,34 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 import RangeSlider from 'react-rangeslider'
 
-const Container = styled.div`
-
-`
-
-class Slider extends React.Component {
-	handleChangeStart = () => {
-	  console.log('Change event started')
-	}	
-
-	handleChangeComplete = () => {
-	  console.log('Change event completed')
-	}
-
-	render() {
-		return (
-			<Container>
-				<RangeSlider
-		      min={1}
-		      max={9}
-		      tooltip={false}
-		      value={this.props.value}
-		      onChangeStart={this.handleChangeStart}
-          onChange={this.props.handleChange}
-          onChangeComplete={this.handleChangeComplete}/>
-			</Container>
-		) 
-	}
-}
-
-export default Slider
+export default props => (
+	<div>
+		<RangeSlider
+			min={1}
+			max={9}
+			tooltip={false}
+			value={props.value}
+		  onChange={props.handleChange}/>
+	</div>
+)
